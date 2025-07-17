2523fadc73a2c52b5ea2265c8e97282221192b6b
Carlos Ismael Orozco Rios <carloselcior@gmail.com>
2025-07-17 14:38:50 -0700

    4.5

---
+++ tests/favoriteblog.test.js
@@ -0,0 +1,13 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+const blogs = require('./blogs')
+
+describe('favorite blog', () => {
+
+  test('of a bigger list is found correctly', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.strictEqual(result, blogs[2])
+  })
+})
